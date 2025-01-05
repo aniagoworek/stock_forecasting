69a079f3d24c18ddeca95e04d35c94ce8b0c6165
Ania <ania.goworek@gmail.com>
2025-01-05 22:43:32 +0100

    Add model performance check

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.stats.diagnostic import acorr_ljungbox
-from arch import arch_model
 import warnings
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.seasonal import seasonal_decompose
 from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import math
 
 from functions import adfuller_test, plot_acf_pacf, kruskal_test, decompose_series
 
@@ -20,7 +21,6 @@ df = pd.read_csv('NKE.csv', header=0)
 print(df.head())
 print(df.describe())
 df = df.drop(['open', 'high', 'low', 'volume'], axis=1)
-# df = df.loc[9354:11095] #9354
 df['date'] = pd.to_datetime(df['date'])
 print(df.head())
 df.set_index('date', inplace=True)
@@ -43,12 +43,12 @@ plt.close()
 
 # Scatter plot
 sns.scatterplot(df['close'])
-plt.title('Nike Stocks from 2018 to 2024')
+plt.title('Nike Stocks from 1980 to 2024')
 plt.savefig('nike_scatterplot.png')
 plt.close()
 
 # Time series decomposition
-decompose_series(df['close'], period=12, filename='time_series_decomposition.png')
+decompose_series(df['close'], period=252, filename='time_series_decomposition.png')
 
 moving_avg = df['log_close'].rolling(12).mean()
 std_dev = df['log_close'].rolling(12).std()
@@ -56,15 +56,17 @@ plt.legend(loc='best')
 plt.title('Moving Average')
 plt.plot(std_dev, color ="black", label = "Standard Deviation")
 plt.plot(moving_avg, color="red", label = "Mean")
+plt.legend()
 plt.savefig('moving average.png') # srednia kroczaca
 plt.close()
 
+# check stationarity after log
 adfuller_test(df['close'])
 adfuller_test(df['log_close'])
 plot_acf_pacf(df['log_close'], 'acf_plot.png', 'pacf_plot.png')
 kruskal_test(df['log_close'], df.index)
 
-# usuwanie sezonowosci i trendu dla stacjonarnosci, zostawienie reszt
+# Delete trend, seasonality, leave residuals (for stationarity)
 result = seasonal_decompose(df['log_close'], model='additive', period=12)
 result.plot()
 plt.savefig('residuals from time series.png')
@@ -72,9 +74,6 @@ plt.close()
 
 residuals = result.resid.dropna()
 adfuller_test(residuals)
-
-print(residuals.head())
-print(residuals.tail())
 
 #split data into train and training set
 train_data, test_data = residuals[3:int(len(residuals)*0.9)], residuals[int(len(residuals)*0.9):]
@@ -87,10 +86,6 @@ plt.plot(test_data, 'blue', label='Test data')
 plt.legend()
 plt.savefig('train and test data.png')
 plt.close()
-
-print(train_data.head())
-print(train_data.tail())
-print("Test Data:", test_data.head())
 
 model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
                       test='adf',       # use adftest to find optimal 'd'
@@ -106,20 +101,19 @@ model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
                       stepwise=True)
 print(model_autoARIMA.summary())
 model_autoARIMA.plot_diagnostics(figsize=(15,8))
-plt.show()
 
 # Best fit for ARIMA model
-model = SARIMAX(train_data, order=(3,0,0), simple_differencing=False) #2,1,1 do log
+model = SARIMAX(train_data, order=(3,0,0), simple_differencing=False)
 model_fit = model.fit(disp=False)
 print(model_fit.summary())
 model_fit.plot_diagnostics(figsize=(10, 8))
-plt.savefig('ARIMA.png')
+plt.savefig('ARIMA (3,0,0) diagnostics.png')
 plt.close()
 
 # Residuals analysis
 residual_model = model_fit.resid
 adfuller_test(residuals)
-ljung_box_test = acorr_ljungbox(residual_model, lags=13) # 24, 36 wyprobowac
+ljung_box_test = acorr_ljungbox(residual_model, lags=10)
 print("Ljung-Box Test Results:")
 print(ljung_box_test)
 if all(ljung_box_test['lb_pvalue'] > 0.05):
@@ -127,7 +121,7 @@ if all(ljung_box_test['lb_pvalue'] > 0.05):
 else:
     print("Residuals are not correlated.")
 
-# Dopasowanie modelu do danych treningowych
+# Model fit for training data
 start_date = pd.to_datetime('1980-12-15 00:00:00')
 end_date = pd.to_datetime('2020-07-01 00:00:00')
 model_predict = model_fit.predict(start=start_date, end=end_date)
@@ -145,43 +139,61 @@ train_data_with_components = train_data + result.trend[train_data.index] + resul
 train_data_with_components = train_data_with_components.dropna()
 
 plt.figure(figsize=(10, 8))
-plt.plot(train_data_with_components[forecast_with_components.index], color='blue', label='Actual (Train Data with Components)')
-plt.plot(forecast_with_components, color='orange', label='Forecast')
+plt.plot(train_data_with_components[forecast_with_components.index], color='orange', label='Actual (Train Data)')
+plt.plot(forecast_with_components, color='blue', label='Forecast')
 plt.fill_between(forecast_with_components.index, lower_limits_train + result.trend[forecast_with_components.index] + result.seasonal[forecast_with_components.index],
                  upper_limits_train + result.trend[forecast_with_components.index] + result.seasonal[forecast_with_components.index],
                  color='pink', alpha=0.3, label='Confidence Interval')
 plt.title("Prediction vs train data", size=24)
 plt.legend()
-plt.savefig('prediction_vs_train_with_components.png')
+plt.savefig('prediction vs train data.png')
 
-# Forecast
+# Forecast for test data
+test_start_date = pd.to_datetime('2020-07-02 00:00:00')
+test_end_date = pd.to_datetime('2024-11-26 00:00:00')
+
+test_start_date = residuals.index[-len(test_data)]
+test_end_date = residuals.index[-1]
+
+test_prediction = model_fit.get_prediction(start=len(train_data), end=len(train_data) + len(test_data) - 1)
+test_confidence_intervals = test_prediction.conf_int()
+test_forecast = test_prediction.predicted_mean
+test_lower_limits = test_confidence_intervals.iloc[:, 0]
+test_upper_limits = test_confidence_intervals.iloc[:, 1]
+
+test_forecast.index = test_data.index
+
+# Add components to test forecast
+test_forecast_with_components = test_forecast + result.trend[test_forecast.index] + result.seasonal[test_forecast.index]
+test_forecast_with_components = test_forecast_with_components.dropna()
 test_data_with_components = test_data + result.trend[test_data.index] + result.seasonal[test_data.index]
 test_data_with_components = test_data_with_components.dropna()
 
-# Forecast
-forecast_result = model_fit.get_forecast(steps=len(test_data))
-fc = forecast_result.predicted_mean
-conf = forecast_result.conf_int(alpha=0.05)
-lower_limits_test = conf.iloc[:, 0]
-upper_limits_test = conf.iloc[:, 1]
+# # Plot forecast vs test data
+plt.figure(figsize=(10, 8))
+plt.plot(test_data_with_components[test_forecast_with_components.index], color='gray', label='Actual (Test Data)')
+plt.plot(test_forecast_with_components, color='blue', label='Forecast')
+plt.title("Prediction vs Test Data", size=24)
+plt.legend()
+plt.savefig('prediction vs test data.png')
+plt.close()
 
-fc_series = pd.Series(fc, index=test_data.index)  # Use the same index as test_data
-lower_series = pd.Series(lower_limits_test.values, index=test_data.index)
-upper_series = pd.Series(upper_limits_test.values, index=test_data.index)
+plt.figure(figsize=(12, 8))
+plt.plot(result.trend.index, result.trend + result.seasonal, color='gray', alpha=0.5, label='Actual data')
+plt.plot(test_data.index, test_data + result.trend[test_data.index] + result.seasonal[test_data.index], color='gray')
+plt.plot(test_forecast.index, test_forecast_with_components, color='blue', label='Forecast')
+plt.axvline(x=test_data.index[0], color='black', linestyle='--')
+plt.title("Nike Stock Forecast", size=24)
+plt.legend()
+plt.savefig('forecast.png')
+plt.close()
 
-#problem diagnosis
-print("Forecast series (fc_series):")
-print(fc_series)
-print("Test Data:", test_data.head())
-
-# Plot
-plt.figure(figsize=(10, 5), dpi=100)
-plt.plot(train_data, label='Training Data')
-plt.plot(test_data, color='blue', label='Actual Stock Price')
-plt.plot(fc_series, color='orange', label='Predicted Stock Price')
-plt.fill_between(lower_series.index, lower_series, upper_series, color='k', alpha=0.10)
-plt.title('ARCH CAPITAL GROUP Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('ARCH CAPITAL GROUP Stock Price')
-plt.legend(loc='upper left', fontsize=8)
-plt.show()
+# Model performance
+mse = mean_squared_error(test_data, test_forecast)
+print('MSE: '+str(mse))
+mae = mean_absolute_error(test_data, test_forecast)
+print('MAE: '+str(mae))
+rmse = math.sqrt(mean_squared_error(test_data, test_forecast))
+print('RMSE: '+str(rmse))
+mape = np.mean(np.abs(test_forecast - test_data)/np.abs(test_data))
+print('MAPE: '+str(mape))
